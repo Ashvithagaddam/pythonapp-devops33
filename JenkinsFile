2a137aeb729735a4a59bb25624266621c25cf264
bunty <buntygaddam22@gmail.com>
2026-04-15 13:03:17 +0530

    Initial commit

---
+++ JenkinsFile
@@ -0,0 +1,16 @@
+pipeline {
+    agent {
+        label 'gfgpython'
+    }
+
+    stages { //collection of jobs
+        stage('Deployment') { //stage == job
+            steps { // job steps
+              sh 'docker pull jinny1/gfgdevops33:latest'
+              sh 'docker rm -f gfgwebos'
+              sh 'docker run -d --name gfgwebos -p 80:80 jinny1/gfgdevops33:latest'
+        }
+            }
+        }
+    }
+   
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        print("inside home function")
+        return 'bye1234567890heheyheyheheyehGFG-33123123'
+
+    @app.route('/test')
+    def test():
+        return 'Hi Sudhanshu test'
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=80, debug=True)
